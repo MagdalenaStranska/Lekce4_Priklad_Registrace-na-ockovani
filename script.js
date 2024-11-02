6ba25896b0534eb224cc5a78ef201c052465c198
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-02 01:00:50 +0100

    Pridavam reseni

--- script.js
+++ script.js
@@ -7,3 +7,17 @@
 // 2. Napište první podmínku, ve které zkontrolujte, že věk uživatele je větší nebo roven 65. Pokud ano, vypište do stránky „věk v pořádku“. Pokud uživateli není alespoň 65 let, vypište „nízký věk“.
 // 3. Napište druhou podmínku, která zkontroluje, zda je zadané heslo delší než osm znaků. Pokud není, vypište „slabé heslo“. Heslo se bude kontrolovat pouze v případě, kdy uživatel splnil první podmínku (věk alespoň 65 let).
 
+const name = prompt("Zadej jméno a příjmení");
+const age = Number(prompt("Zadej věk"));
+const password = prompt("Zadej heslo");
+
+if (age >= 65) {
+    document.body.innerHTML += '<p>Věk je v pořádku.</p>';
+    if (password.length <= 8) {
+        document.body.innerHTML += '<p>Slabé heslo.</p>';
+    } else {
+        document.body.innerHTML += '<p>Heslo je v pořádku.</p>';
+    }
+} else {
+    document.body.innerHTML += '<p>Nízký věk.</p>';
+}
